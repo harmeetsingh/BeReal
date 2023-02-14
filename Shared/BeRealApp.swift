b3639485ab0a51385369995e0550611952bae900
Harmeet Singh <singh_120@hotmail.co.uk>
2023-02-14 13:51:25 +0000

    Initial Commit

---
+++ Shared/BeRealApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeRealApp.swift
+//  Shared
+//
+//  Created by Harmeet Singh on 14/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct BeRealApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
